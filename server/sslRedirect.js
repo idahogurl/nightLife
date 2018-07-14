89c29fe7861706ff5893dd83737e0b259d963fb9
Rebecca Vest <rebecca.vest@stukent.com>
2018-07-13 23:24:23 -0600

    Another try

--- server/sslRedirect.js
+++ server/sslRedirect.js
@@ -3,6 +3,7 @@ export default function (req, res, next) {
   console.log('url', req.url);
   console.log('secure', req.secure);
   console.log('protocol', req.protocol);
+  console.log('header', req.headers['X-Forwarded-Proto']);
   if (process.env.NODE_ENV === 'production' && req.url !== '/graphql' && !req.secure) {
     const sslUrl = `https://${req.hostname}${req.url}`;
     console.log('redirect', sslUrl);
@@ -11,3 +12,4 @@ export default function (req, res, next) {
 
   return next();
 }
+
