4504a025e71a842a08b91e1371771c66495d019c
Rebecca Vest <rebecca.vest@stukent.com>
2018-07-18 21:51:58 -0600

    Try #4

--- server/server.babel.js
+++ server/server.babel.js
@@ -12,12 +12,12 @@ import yelpRequest from './yelp';
 require('dotenv').config();
 
 const app = express();
+const router = express.Router();
 
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'development';
 
 app.set('trust proxy');
-app.use(sslRedirect);
 app.use('/', express.static('public'));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,24 +26,21 @@ app.use(bodyParser.json());
 const typeDefs = fs.readFileSync(path.resolve(__dirname, 'graphql/schema.gql'), 'utf8');
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-app.use('/graphql', graphqlExpress(req => ({ context: req.user, schema })));
+router.use('/graphql', graphqlExpress(req => ({ context: req.user, schema })));
 if (env === 'development') {
-  app.get('/graphiql', graphiqlExpress({
+  router.get('/graphiql', graphiqlExpress({
     endpointURL: '/graphql',
   }));
 }
 
-app.get('/yelp', (req, res, next) => {
+router.get('/yelp', (req, res, next) => {
   yelpRequest(req, res, next);
 });
 
 // Always return the main index.html, so react-router renders the route in the client
-app.get('*', (req, res) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    res.redirect(302, `https://${req.hostname}${req.originalUrl}`);
-  } else {
-    res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
-  }
+router.get('*', (req, res, next) => {
+  sslRedirect(req, res, next);
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
 });
 
 app.listen(port, () => {
