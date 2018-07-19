6bf4dd5d7592a0b3a908ebd33b5ce2694bd1b309
Rebecca Vest <dynamicdiner@gmail.com>
2018-07-18 23:33:54 -0600

    Force HTTPs via Javascript (#1)

--- server/server.babel.js
+++ server/server.babel.js
@@ -5,7 +5,6 @@ import path from 'path';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import fs from 'fs';
 import { makeExecutableSchema } from 'graphql-tools';
-import sslRedirect from './sslRedirect';
 import resolvers from './graphql/resolvers';
 import yelpRequest from './yelp';
 
@@ -21,7 +20,6 @@ app.use('/', express.static('public'));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(sslRedirect);
 
 const typeDefs = fs.readFileSync(path.resolve(__dirname, 'graphql/schema.gql'), 'utf8');
 const schema = makeExecutableSchema({ typeDefs, resolvers });
@@ -38,12 +36,8 @@ app.get('/yelp', (req, res, next) => {
 });
 
 // Always return the main index.html, so react-router renders the route in the client
-app.get('*', (req, res) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    res.redirect(302, `https://${req.hostname}${req.originalUrl}`);
-  } else {
-    res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
-  }
+app.get('*', (_, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
 });
 
 app.listen(port, () => {
